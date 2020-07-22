262f13ddd381098252238f6ae2046ad3735125e4
lvah <976131979@qq.com>
2020-07-22 20:38:39 +0800

    第3次提交: Admin后台管理与模型的注册

--- bookApp/admin.py
+++ bookApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, Hero
 
+# 后台管理的配置文件
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Hero)
--- bookApp/models.py
+++ bookApp/models.py
@@ -9,13 +9,20 @@ class Book(models.Model):
     # title是字符串类型的， 并且最大的长度为20;
     title = models.CharField(max_length=20)
     pub_date = models.DateField()
-
+    # 字符串的友好展示, 在交互式环境测试的时候生效
     def __repr__(self):
         return '<Book: %s>' % (self.title)
+    # 字符串友好展示，在后台管理的时候生效
+    def __str__(self):
+        return self.title
 
     # 自定义对应的表名，默认表名：bookApp_book
     class Meta:
         db_table = "books"
+        # 后台管理显示（单数）
+        verbose_name = "图书管理"
+        # 后台管理显示（复数）
+        verbose_name_plural = "图书管理"
 
 
 class Hero(models.Model):
@@ -29,6 +36,14 @@ class Hero(models.Model):
 
     def __repr__(self):
         return "<Hero %s>" % (self.name)
+
+    def __str__(self):
+        return self.name
+
     # 自定义对应的表名，默认表名：bookApp_book
     class Meta:
         db_table = "heros"
+        # 后台管理显示（单数）
+        verbose_name = "人物管理"
+        # 后台管理显示（复数）
+        verbose_name_plural = "人物管理"
